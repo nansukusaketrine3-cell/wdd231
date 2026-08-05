802d7843643a4acf470ff104a5dcdcbcce4271da
nansukusaketrine3-cell <nansukusaketrine3@gmail.com>
2026-08-05 20:11:24 +0300

    commit

---
+++ site project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+// Copyright Year
+
+const copyright = document.querySelector("#copyright");
+
+copyright.innerHTML =
+`&copy; ${new Date().getFullYear()} | Nansukusa Ketrine | WDD 231 Individual Website Project`;
